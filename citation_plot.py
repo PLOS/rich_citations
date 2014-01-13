09f991c0b58fd967814dabc4a21dea10d32cec45
Adam Becker <abecker@plos.org>
2014-01-13 10:38:53 -0800

    Added plotting script.

---
+++ citation_plot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Code written by Adam Becker
+# abecker@plos.org
+
+
+"""
+citation_plot.py
+
+Created by Adam M Becker on 2014-01-04.
+Released under a Creative Commons CC-BY 4.0 license (cc) 2014. Some rights reserved.
+"""
+
+from __future__ import division
+import pickle
+from pylab import *
+
+DATABASE_FILE = "circadian_database.pickle"
+
+# Open the database.
+f = open(DATABASE_FILE, "r")
+db = pickle.load(f)
+f.close()
+
+print "Number of papers in database is " + str(len(db)) + "."
+
+citation_range = (5, 10)
+papers_in_range = filter(lambda x: x["citations"] >= citation_range[0] and x["citations"] <= citation_range[1], db.values())
+print "Number of papers with at least " + str(citation_range[0]) + " citations and no more than " + str(citation_range[1]) + " citations is " + str(len(papers_in_range)) +"."
+mean_mentions = [mean(p["ipcs"]) for p in papers_in_range]
+bins = linspace(1, 10, 19)
+hist(mean_mentions, bins)
+xticks(size = 30)
+yticks(size = 30)
+ylabel(r"Number of papers ($n = 431$)", size = 40)
+xlabel("Mean number of mentions within a citing paper", size = 40)
+show()
