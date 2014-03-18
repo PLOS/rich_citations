52a896cdf17f782a87517b6abfa76b6d69252eed
Marc Rohloff <marc@marcrohloff.com>
2014-03-18 11:32:00 -0600

    [#67585630] Code for performance testing the cross ref API

---
+++ script/crossref_performance_test.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ./bin/rails runner
+
+Rails.logger = Logger.new($stdout)
+
+require 'pp'
+require 'benchmark'
+
+DOI ='10.1371/journal.pone.0046843'
+
+xml = Plos::Api.document( DOI )
+parser = Plos::PaperParser.new(xml)
+
+class Plos::PaperParser
+  def reference_nodes_text
+    @reference_nodes_text ||= reference_nodes.map { |n| n.text }
+  end
+end
+
+search_texts = parser.reference_nodes_text
+puts "DOI #{DOI} has #{search_texts.count} reference nodes"
+
+# Benchmark.bmbm do |bm|
+#   (0..10).each do |n|
+#     nn = n*3+1
+#     refs = search_texts[ 0..(nn-1) ]
+#     bm.report("#{nn} nodes #{refs.length}") {
+#       Plos::Api.cross_refs(refs)
+#     }
+#   end
+# end
+
+puts '----------------------------'
+puts  'refs >    total   per.ref'
+puts '----------------------------'
+iterations = 5
+(0..16).each do |n|
+  nn = n*3+1
+  refs = search_texts[ 0..(nn-1) ]
+  total = Benchmark.realtime do
+    iterations.times {
+     Plos::Api.cross_refs(refs)
+    }
+  end
+  per_iteration = total / iterations
+  per_ref = per_iteration / nn
+  puts '%2d -->  %5f %5f' % [nn, per_iteration, per_ref]
+end
+puts '----------------------------'
+puts  'refs >    total   per.ref'
+puts '----------------------------'
+
