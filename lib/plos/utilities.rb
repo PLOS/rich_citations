7144e90867c198b3b6985e657f2f728676cd3a6a
Marc Rohloff <marc@marcrohloff.com>
2014-04-11 11:18:56 -0600

    [#69143592] Fixed punctuation regexes for DOI

--- lib/plos/utilities.rb
+++ lib/plos/utilities.rb
@@ -1,12 +1,12 @@
 
 module Plos
   class Utilities
-    PUNCT = %q{[]'".,:!?)-;/`]}  # Posix [[:punct:]] regex is more liberal than we want
+    PUNCT = %q{[\]'"`.,:;!)\-\/]} # Posix [[:punct:]] regex is more liberal than we want
 
     # (^|\s)doi:?\s*(?<result>10\.([[:punct:]]*[^[[:punct:]][[:space:]]]+)+)
 
-    DOI_PREFIX_REGEX = /(^|\s)doi:?\s*(?<result>10\.\S+(?<![[:punct:]]))/io
-    DOI_URL_REGEX    = /(^|\W)doi\.org\/(?<result>10\.\S+(?<![[:punct:]]))/io
+    DOI_PREFIX_REGEX = /(^|\s)doi:?\s*(?<result>10\.\S+(?<!#{PUNCT}))/io
+    DOI_URL_REGEX    = /(^|\W)doi\.org\/(?<result>10\.\S+(?<!#{PUNCT}))/io
 
     def self.extract_doi(text)
       match(text, { DOI_URL_REGEX    => true,
