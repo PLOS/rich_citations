4afcc5ea670dc96468b48b2f2afe4801d154285d
Erik Hetzner <ehetzner@plos.org>
2014-05-08 16:46:24 -0700

    rewrite to use a single data structure to hold JSON data and HTML reference list

--- app/assets/javascripts/references.js.jsx
+++ app/assets/javascripts/references.js.jsx
@@ -10,12 +10,17 @@ var idx = lunr(function () {
 });
 
 function buildIndex(references) {
-    $.makeArray(references['_ordered']).map(function (ref) {
+    for (var id in references) {
+        var ref = references[id];
         var doc = { id:    ref.id,
                     title: ref.info.title,
                     body:  ref.text };
         idx.add(doc);
-    });
+    }
+}
+
+function getReferenceId (el) {
+    return $("a:first", el).attr('id');
 }
 
 /** 
@@ -34,79 +39,15 @@ function buildReferenceData(json, elements) {
         retval[v.id]['html'] = $(elementsById[v.id]).html();
         retval[v.id]['text'] = $(elementsById[v.id]).text();
     });
-    retval['_ordered'] = [];
+    var i = 0;
     $("ol.references li").each(function (el) {
-        retval['_ordered'].push(retval[getReferenceId($(this))]);
+        var id = getReferenceId($(this));
+        retval[id]['index'] = i;
+        i = i + 1;
     });
     return retval;
 }
 
-function getRefListById(json) {
-    var l = {};
-    $.each(json['references'], function (ignore, v) {
-        l[v.id] = v;
-    });
-    return l;
-}
-
-function getReferenceInfoById(refs, id) {
-    if (refs[id] == null) {
-        return null;
-    } else {
-        return refs[id].info;
-    }
-}
-
-/**
- * Build a function that can be passed to filter to remove items that
- * cannot be sorted.
- */
-function mkUnsortableFilter(by, json) {
-    var refs = getRefListById(json);
-    return function (el) {
-        var info = getReferenceInfoById(refs, getReferenceId(el));
-        if (info === null) {
-            return true;
-        }
-        else {
-            return getReferenceInfoField(info, by) === null;
-        }
-    };
-}
-
-function getReferenceInfoField(info, fieldname) {
-    var s = info[fieldname];
-    if (!s || (typeof myVar != 'undefined') || s === "") {
-        return null;
-    } else {
-        return s;
-    }
-}
-
-function getReferenceId (el) {
-    return $("a:first", el).attr('id');
-}
-
-/**
- * Build a function from a field and a JSON data structure as returned
- * by richcites API that can be passed to sort to sort an array of
- * references.
- */
-function mkSortRefListFunction (by, json, descending=false) {
-    var refs = getRefListById(json);
-    var pipeline = new lunr.Pipeline();
-    pipeline.add(lunr.trimmer,
-                 lunr.stopWordFilter);
-    return function (a, b) {
-        var aref = getReferenceInfoById(refs, getReferenceId(a));
-        var bref = getReferenceInfoById(refs, getReferenceId(b));
-        var aval = getReferenceInfoField(aref, by);
-        var bval = getReferenceInfoField(bref, by);
-        // XXX too many calls to pipeline
-        return pipeline.run(lunr.tokenizer(aval)).join(" ").localeCompare(pipeline.run(lunr.tokenizer(bval)).join(" "));
-    };
-}
-                            
 var Reference = React.createClass({
     render: function () {
         return <li dangerouslySetInnerHTML={{__html:this.props.html}} />;
@@ -114,29 +55,98 @@ var Reference = React.createClass({
 });
 
 var SortedReferencesList = React.createClass({
-    render: function() {
-        var refsArray = $.makeArray(this.props.references);
-        var sorted, unsorted;
-
-        /* Index is special, we just use the original sort order. */
-        if (this.props.current.by === 'index') {
-            sorted = refsArray.filter(this.props.searchResultsFilter);
-            unsorted = [];
-        } else {
-            var unsortableFilter = mkUnsortableFilter(this.props.current.by, this.props.json);
-            var sortableFilter = function (el) { return !unsortableFilter(el); };
-            var sortFunc = mkSortRefListFunction(this.props.current.by, this.props.json);
-            unsorted = refsArray.filter(unsortableFilter).
-                filter(this.props.searchResultsFilter);
-            sorted = refsArray.filter(sortableFilter).sort(sortFunc).
-                filter(this.props.searchResultsFilter);
+    /**
+     * Build a function that can be passed to filter to remove items that
+     * cannot be sorted.
+     */
+    mkUnsortableFilter: function(extractor) {
+        return function (el) {
+            var v = extractor(el);
+            if (typeof(v) === "undefined" || v === null || v === "") {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    },
+    /**
+     * sort definitions, consists of a function to extract the field
+     * to sort and a type, either "numeric" or "alphabetic"
+     */
+    sorts: {
+        index: {
+            extractor: function (ref) {
+                return ref.index;
+            },
+            sortStyle: "numeric"
+        },
+        title: {
+            extractor: function (ref) {
+                return ref.info.title;
+            },
+            sortStyle: "alphabetic"
+        },
+        year: {
+            extractor: function (ref) {
+                return parseInt(ref.info.year);
+            },
+            sortStyle: "numeric"
+        },
+        author: {
+            extractor: function(ref) {
+                return ref.info.authors[0].fullname;
+            },
+            sortStyle: "alphabetic"
+        },
+        mentions: {
+            extractor: function(ref) {
+                return ref.mentions;
+            },
+            sortStyle: "numeric"
+        },
+        journal: {
+            extractor: function(ref) {
+                return ref.info.journal;
+            },
+            sortStyle: "alphabetic"
         }
+        
+    },
+    /**
+     * Build a sort function from a sort definition, as defined in `sorts` above.
+     */
+    mkSortFunc: function(o) {
+        var extractor = o.extractor;
+        if (o.sortStyle === "numeric") {
+            return function (a,b) {
+                return extractor(a) - extractor(b);
+            };
+        } else {
+            var pipeline = new lunr.Pipeline();
+            pipeline.add(lunr.trimmer,
+                         lunr.stopWordFilter);
+            return function (a, b) {
+                var aval = pipeline.run(lunr.tokenizer(extractor(a))).join(" ");
+                var bval = pipeline.run(lunr.tokenizer(extractor(b))).join(" ");
+                return aval.localeCompare(bval);
+            };
+        }
+    },
+    render: function() {
+        var refsArray = $.map(this.props.references, function(val, key) { return val; });
+        var unsortableFilter = this.mkUnsortableFilter(this.sorts[this.props.current.by].extractor);
+        var sortableFilter = function (el) { return !unsortableFilter(el); };
+        var unsorted = refsArray.filter(unsortableFilter).
+            filter(this.props.searchResultsFilter);
+        var sorted = refsArray.filter(sortableFilter).
+                sort(this.mkSortFunc(this.sorts[this.props.current.by])).
+                filter(this.props.searchResultsFilter);
         
         if (this.props.current.order == "desc") { sorted = sorted.reverse(); }
 
         /* Build elements for react */
-        var mkElementFunc = function (h) {
-            return <Reference html={$(h).html()} key={getReferenceId(h)} />;
+        var mkElementFunc = function (ref) {
+            return <Reference html={ref.html} key={ref.id} />;
         };
         return <div>
             <ol className="references">{ sorted.map(mkElementFunc) }</ol>
@@ -202,10 +212,10 @@ var ReferencesApp = React.createClass({
         if (this.state.filterText) {
             var resultHash = {};
             idx.search(this.state.filterText).map(function (res) {
-                return resultHash[res['ref']] = res['score'];
+                resultHash[res['ref']] = res['score'];
             });
-            return function (e) {
-                return (resultHash[getReferenceId(e)] != null);
+            return function (ref) {
+                return (resultHash[ref.id] != null);
             };
         } else {
             /* by default return all results */
@@ -216,12 +226,14 @@ var ReferencesApp = React.createClass({
         return <div>
             <SearchBar filterText={this.state.filterText} onSearchUpdate={this.handleSearchUpdate}/>
             <ul>
-            <li><Sorter name="Index"  by="index"  current={this.state.sort} onClick={this.handleSorterClick}/></li>
-            <li><Sorter name="Title"  by="title"  current={this.state.sort} onClick={this.handleSorterClick}/></li>
-            <li><Sorter name="Author" by="author" current={this.state.sort} onClick={this.handleSorterClick}/></li>
-            <li><Sorter name="Year"   by="year"   current={this.state.sort} onClick={this.handleSorterClick}/></li>
+            <li><Sorter name="Index"    by="index"    current={this.state.sort} onClick={this.handleSorterClick}/></li>
+            <li><Sorter name="Title"    by="title"    current={this.state.sort} onClick={this.handleSorterClick}/></li>
+            <li><Sorter name="Author"   by="author"   current={this.state.sort} onClick={this.handleSorterClick}/></li>
+            <li><Sorter name="Year"     by="year"     current={this.state.sort} onClick={this.handleSorterClick}/></li>
+            <li><Sorter name="Mentions" by="mentions" current={this.state.sort} onClick={this.handleSorterClick} defaultOrder="desc"/></li>
+            <li><Sorter name="Journal"  by="journal"  current={this.state.sort} onClick={this.handleSorterClick}/></li>
             </ul>
-            <SortedReferencesList current={this.state.sort} references={this.props.references} searchResultsFilter={this.mkSearchResultsFilter()} json={this.props.json}/>
+            <SortedReferencesList current={this.state.sort} references={this.props.references} searchResultsFilter={this.mkSearchResultsFilter()} />
             </div>;
     }
 });
@@ -231,6 +243,7 @@ $(document).ready(function () {
     var doi = $('meta[name=citation_doi]').attr("content");
     /* now fetch the JSON describing the paper */
     $.getJSON("/papers/" + doi + "?format=json", function(data) {
+        /* build main data structure */
         var references = buildReferenceData(data);
         /* build full-text index */
         buildIndex(references);
@@ -238,7 +251,7 @@ $(document).ready(function () {
         $("<div id='richcites'></div>").insertBefore("#references");
         /* and drop into react */
         React.renderComponent(
-            <ReferencesApp references={$("ol.references li")} json={data}/>,
+            <ReferencesApp references={references} />,
             $("ol.references").get(0)
         );
     });
