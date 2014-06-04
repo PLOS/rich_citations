a3ab288a7dc84415584cb0d79191a554b38fd837
Erik Hetzner <ehetzner@plos.org>
2014-06-04 09:40:29 -0700

    turn caching off in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+
 CitationTools::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -11,7 +12,7 @@ CitationTools::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
