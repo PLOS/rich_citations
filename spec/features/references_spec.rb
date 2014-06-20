88e498c78fadec92216676e15a32753942fcbd95
Erik Hetzner <ehetzner@plos.org>
2014-06-20 14:25:07 -0700

    fix spec tests

--- spec/features/references_spec.rb
+++ spec/features/references_spec.rb
@@ -1,6 +1,8 @@
 # coding: utf-8
 require "spec_helper"
 
+Capybara.default_wait_time = 60
+
 describe "reference viewing", :type => :feature, :js => true do
   it "should properly sort a references list" do
     visit '/view/10.1371/journal.pone.0067380'
@@ -9,7 +11,7 @@ describe "reference viewing", :type => :feature, :js => true do
     page.click_button("Title")
     expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Simon1")
 
-    page.click_button("Mentions")
+    page.click_button("Appearances")
     expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Domenici1")
 
     page.click_button("Year")
@@ -30,7 +32,7 @@ describe "reference viewing", :type => :feature, :js => true do
     # should be disabled on non-appearance sorts
     page.click_button("Title")
     expect(page).to have_button("Show repeated citations", disabled: true)
-    page.click_button("Mentions")
+    page.click_button("Appearances")
     expect(page).to have_button("Show repeated citations", disabled: true)
     page.click_button("Year")
     expect(page).to have_button("Show repeated citations", disabled: true)
@@ -43,7 +45,7 @@ describe "reference viewing", :type => :feature, :js => true do
 
   it "should have a group citations button" do
     visit '/view/10.1371/journal.pone.0067380'
-
+    
     # should not be visible until Show repeated citations toggled
     expect(page).to have_button("Group citations", disabled: true)
 
@@ -54,6 +56,10 @@ describe "reference viewing", :type => :feature, :js => true do
   it "should display a retracted mark for retracted cites" do
     visit '/view/10.1371/journal.pone.0059428'
 
-    expect(page).to have_content("Small Protein-Mediated Quorum Sensing in a Gram-Negative Bacterium PLoS ONE RETRACTED")
+    expect(page).to have_content("Small Protein-Mediated Quorum Sensing in a Gram-Negative Bacterium PLoS ONE 10.1371/journal.pone.0029192 RETRACTED")
   end
+
+  it "should work when a reference is not actually cited in the paper" do
+    visit '/view/10.1371/journal.pone.0100115'
+  end    
 end
