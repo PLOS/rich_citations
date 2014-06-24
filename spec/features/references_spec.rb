2dc12c8b81371da961bc4984fb2e660173d00500
Erik Hetzner <ehetzner@plos.org>
2014-06-24 11:23:44 -0700

    fix spec for new ui

--- spec/features/references_spec.rb
+++ spec/features/references_spec.rb
@@ -11,8 +11,11 @@ describe "reference viewing", :type => :feature, :js => true do
     page.click_button("Title")
     expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Simon1")
 
-    page.click_button("Appearances")
-    expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Domenici1")
+    page.click_button("Order in paper")
+    expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Clua1")
+
+    page.click_button("Order in paper with repeated")
+    expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Clua1")
 
     page.click_button("Year")
     expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Sperone2")
@@ -22,35 +25,9 @@ describe "reference viewing", :type => :feature, :js => true do
 
     page.click_button("Journal")
     expect(page.first(:xpath, '//ol[@class="references"]//ol/li/div')['id']).to eq("reference_pone.0067380-ODonoghue1")
-  end
 
-  it "should have a show repeated citations button" do
-    visit '/view/10.1371/journal.pone.0067380'
-    
-    expect(page).to have_button("Show repeated citations")
-
-    # should be disabled on non-appearance sorts
-    page.click_button("Title")
-    expect(page).to have_button("Show repeated citations", disabled: true)
     page.click_button("Appearances")
-    expect(page).to have_button("Show repeated citations", disabled: true)
-    page.click_button("Year")
-    expect(page).to have_button("Show repeated citations", disabled: true)
-    page.click_button("Author")
-    expect(page).to have_button("Show repeated citations", disabled: true)
-    page.click_button("Journal")
-    expect(page).to have_button("Show repeated citations", disabled: true)
-
-  end
-
-  it "should have a group citations button" do
-    visit '/view/10.1371/journal.pone.0067380'
-    
-    # should not be visible until Show repeated citations toggled
-    expect(page).to have_button("Group citations", disabled: true)
-
-    page.click_button("Show repeated citations")
-    expect(page).to have_button("Group citations")
+    expect(page.first(:xpath, '//ol[@class="references"]/li/div')['id']).to eq("reference_pone.0067380-Domenici1")
   end
 
   it "should display a retracted mark for retracted cites" do
