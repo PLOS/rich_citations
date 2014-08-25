0e2518ec967b7e0826ddd6a128652bf0a254242b
Erik Hetzner <ehetzner@plos.org>
2014-08-25 13:50:08 -0700

    add read API 404s for non-PLOS DOIs

--- app/controllers/api_v0_controller.rb
+++ app/controllers/api_v0_controller.rb
@@ -6,10 +6,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+# 
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-#
+# 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,27 +18,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-CitationTools::Application.routes.draw do
-
-  root 'papers#index'
-
-  get '', to:'results#index', as:'search'
-
-  resources :results, only:[:index, :show] do
-    member do
-      get '/cited/:ref', action:'cited', constraints: {ref: /.+/}, format:false, as:'cited'
-    end
-    collection do
-      post :search
-      post :list
-      post :parse
+class ApiV0Controller < ApplicationController
+  def paper
+    id = params[:id]
+    case id
+    when %r{^http://dx.doi.org/10.1371/}
+    else
+      render status: 404, text: ''
     end
   end
-
-  resources :papers, only:[:show], format:false, id: /.+/
-  resources :references, only:[:show], format:false, id: /.+/
-  get '/view/:id/references/:referenceid', to: 'papers#reference', id:/.+/
-  get '/view/:id', to: 'papers#view', id:/.+/ 
-  get '/interstitial', to: 'papers#interstitial'
-  get '/v0/paper', to: 'api_v0#paper'
 end
