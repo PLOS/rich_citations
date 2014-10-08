036a93e6a59e368b289d057c50e36d65a19f3a5a
Erik Hetzner <ehetzner@plos.org>
2014-10-08 14:27:20 -0700

    rewrite cross ref query per python script to send https://github.com/PLOS/developer-scripts/blob/master/citedArticle/populateCitationDoiFromCrossRef.py

--- app/classes/identifier_resolvers/cross_ref.rb
+++ app/classes/identifier_resolvers/cross_ref.rb
@@ -26,7 +26,12 @@ module IdentifierResolvers
     API_URL = 'http://search.crossref.org/links'
 
     def resolve
-      unresolved_texts = unresolved_references.map { |id, data| [id, data.text] }.to_h
+      unresolved_texts = unresolved_references.map { |id, data|
+        parsed = CrossRef.parse_ref_journal(data.node)
+        text = CrossRef.format_for_crossref(parsed)
+        text = data.text if text.blank?
+        [id, text]
+      }.to_h
       @crossref_infos = state[:crossref_infos] = {}
       unresolved_texts.each_slice(SLICE_SIZE) do |group|
         # if it failed, try each individual item
@@ -39,6 +44,68 @@ module IdentifierResolvers
     end
 
     private
+
+    def self.parse_ref_journal(ref)
+      data = {}
+      names = []
+      ref.xpath('.//name').each do |name_node|
+        name = {}
+        # get surname and given-names
+        surname_text = name_node.xpath('surname').text
+        name['surname'] = surname_text unless surname_text.blank?
+        given_names_text = name_node.xpath('given-names').text
+        name['given-names'] = given_names_text unless given_names_text.blank?
+        names.append(name)
+      end
+      ref.xpath('.//collab').each do |name_node|
+        names.append('collab' => name_node.text)
+      end
+      data['names'] = names
+
+      ['article-title', 'source', 'volume', 'issue', 'fpage',
+       'lpage', 'elocation-id', 'year'].each do |n|
+        txt = ref.xpath(".//#{n}").text
+        data[n] = txt unless txt.blank?
+      end
+      data
+    end
+
+    def self.format_for_crossref(ref_data)
+      # https://github.com/PLOS/developer-scripts/blob/master/citedArticle/populateCitationDoiFromCrossRef.py
+      crossref_data = ''
+      names = []
+      ref_data['names'].each do |name|
+        name_data = ''
+        if (name.key?('surname') && name.key?('given-names'))
+          surname = name['surname']
+          given_name = name['given-names']
+          if (given_name.match(/\p{Upper}/))
+            # format the given name Example: J. or J. D.
+            name_data = given_name.split(//).map { |c| "#{c}." }.join(' ') + ' ' + surname
+          else
+            # assuming that this is a full given name
+            name_data = given_name + ' ' + surname
+          end
+        elsif name.key?('surname')
+          name_data = name['surname']
+        elsif name.key?('collab')
+          name_data = name['collab']
+        end
+        names.append(name_data) unless name_data.blank?
+        crossref_data = names.join(', ')
+      end
+      crossref_data << ", \"#{ref_data['article-title']}\"" if (ref_data.key?('article-title'))
+      crossref_data << ", #{ref_data['source']}" if ref_data.key?('source')
+      crossref_data << ", vol. #{ref_data['volume']}" if ref_data.key?('volume')
+      crossref_data << ", no. #{ref_data['issue']}" if ref_data.key?('issue')
+      if ref_data.key?('fpage')
+        crossref_data << ", pp. #{ref_data['fpage']}"
+        crossref_data << "-#{ref_data['lpage']}" if ref_data.key?('lpage')
+      end
+      crossref_data << ", e#{ref_data['elocation-id']}" if ref_data.key?('elocation-id')
+      crossref_data << ", #{ref_data['year']}" if ref_data.key?('year')
+      crossref_data
+    end
 
     # Results with a lower score from the crossref.org will be ignored
     MIN_CROSSREF_SCORE = 2.5 #@TODO: Keeping this value high to force skips for testing
@@ -79,10 +146,10 @@ module IdentifierResolvers
       return nil unless doi.present?
 
       {
-          uri_source:   :crossref,
-          uri_type:     :doi,
-          uri:          "http://dx.doi.org/#{doi}",
-          score:        result['score'].to_f,
+        uri_source:   :crossref,
+        uri_type:     :doi,
+        uri:          "http://dx.doi.org/#{doi}",
+        score:        result['score'].to_f,
       }
     end
 
