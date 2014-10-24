b9769ddc158aba98f183c518a7157d2f06cd732e
Erik Hetzner <ehetzner@plos.org>
2014-10-24 15:02:47 -0700

    increase db connection pool

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,6 +18,11 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+             Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['DB_POOL'] || 20
+    ActiveRecord::Base.establish_connection
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
