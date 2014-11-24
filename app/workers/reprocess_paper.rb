e3086c5174a37d6c581239abba94621af9e990a4
Erik Hetzner <ehetzner@plos.org>
2014-11-24 11:53:32 -0800

    fix class name

--- app/workers/reprocess_paper.rb
+++ app/workers/reprocess_paper.rb
@@ -2,7 +2,7 @@ class ReprocessPaper
   include Sidekiq::Worker
 
   def perform(doi)
-    paper = Paper.find_by(doi: doi)
+    paper = PaperResult.find_by(doi: doi)
     paper.destroy!
     AnalyzePaper.perform_async(doi)
   end
